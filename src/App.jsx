d51be4e49761e898bc08c92c567b4d3d78f2aa1b
Elano Silva <elano_16@yahoo.com.br>
2024-02-01 22:28:36 -0300

    Finalizando Footer e começando o conteudo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import './App.css'
 import Header from "./components/header";
+import Footer from "./components/Footer";
 
-;function App() {
+function App() {
   return (
     <>
     <Header />
+    <section className="container">
+      <div className="apresentacao">
+        <p>Olá sou <br />
+        <span>Elano Silva</span> <br />
+        Dev Full Stack</p>
+        <button className="btn btn-red">Saiba mais sobre mim</button>
+      </div>
+      <figure>
+        <img className="img-home" src="/developer-red.svg" alt="Imagem de Home"/>
+      </figure>
+    </section>
+    <Footer />
     </>
   );
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import styles from './Footer.module.css'
+
+
+function Footer() {
+    return (
+        <footer className={styles.Footer}>
+            © 2024 powered by Elano.
+        
+        </footer>
+
+
+    );
+}
+
+
+export default Footer
