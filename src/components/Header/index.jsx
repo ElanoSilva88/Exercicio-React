897de78f7b5964e51f51f47e4d9c83f59e73adc4
Elano Silva <elano_16@yahoo.com.br>
2024-02-05 14:40:31 -0300

    Menu Sandwich

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,34 @@
+import { useState } from 'react'
 import styles from './Header.module.css'
 
 function Header(){
+const [ showMenu, setShowMenu ] = useState(false)
+const toggleMenu = () => {
+    setShowMenu(!showMenu)
+}
+
+
+
     return(
         <header className={styles.header}>
-            <span>Elano Silva</span>
-            <nav>
+            <a href=""><span>Elano Silva</span>
+            </a>
+            <nav 
+            className={`${styles.menuSandwich} ${ showMenu ? styles.show : ''}`}
+            onClick={toggleMenu}
+            >
                 <a href="">Home</a>
                 <a href="">Sobre</a>
                 <a href="">Projetos</a>
                 <a href="">Contatos</a>
             </nav>
+            <div className={styles.menuButton}
+                onClick={toggleMenu}
+                >
+                <span className={styles.linha}></span>
+                <span className={styles.linha}></span>
+                <span className={styles.linha}></span>
+            </div>
         </header>
 
     )
